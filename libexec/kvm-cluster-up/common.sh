83a2d11a1663cb572a018e1d0250f0cc2d0d85e5
Andrew McDermott <aim@frobware.com>
2018-02-21 08:56:19 +0000

    Fix source locations in libexec/kvm-cluster-up/create-config-drive

--- libexec/kvm-cluster-up/common.sh
+++ libexec/kvm-cluster-up/common.sh
@@ -18,8 +18,8 @@ fi
 : ${KUP_OS_VARIANT:?}
 : ${KUP_CLOUD_USERNAME:?}
 
-: ${KUP_DISK_RESIZE:=10}
-: ${KUP_DISK_SIZE:=5}
+: ${KUP_DISK_RESIZE:=50}
+: ${KUP_DISK_SIZE:=50}
 : ${KUP_NCPUS:=2}
 : ${KUP_RAM:=4096}
 : ${KUP_POOL:=default}
